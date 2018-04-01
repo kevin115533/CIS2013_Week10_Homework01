80e6416455239ff1a3878f27fa32d36d296f52e0
kevin115533 <35547586+kevin115533@users.noreply.github.com>
2018-04-01 18:16:26 -0500

    week 10 homework
    add field codes for user input

--- CIS2013_Week10_Homework01.cpp
+++ CIS2013_Week10_Homework01.cpp
@@ -4,26 +4,33 @@ using namespace std;
 
 int main() {
 	char stay;
-	int *a, *b;
-	int boardWid = 0;
-	int boardLen = 0;
+	int *a,*b;
+	int columns;
+	int rows;
 
 	cout << "Welcome to Minesweeper" << endl;
-	cout << "How wide do you want the the field to be? ";
-	cin >> boardWid;
+	cout << "How many columns do you want the the field to be? ";
+	cin >> columns;
 
-	cout << "How tall do you want the field to be? ";
-	cin >> boardLen;
+	cout << "How many rows do you want the field to be? ";
+	cin >> rows;
 
-	a = new int[boardWid];
-	b = new int[boardLen];
+	a = new int [columns];
+	b = new int[rows];
+	
+	cout << "  ";
+	for (int x = 0; x < columns; x++) {
+		cout << x+1 << " ";
+	}
+	cout << endl;
 
-	for (int x = 0; x < boardWid; x++) {
-		cout << x << " ";
-		/*for (int y = 0; y < boardLen; y++) {
-			cout << y;
-		}*/
+	for (int x = 0; x < rows; x++) {
+		cout << x+1 << " ";
+		for (int y = 0; y < columns; y++) {
+			cout << ". ";
 		}
+		cout << endl;
+	}
 	
 
 	cin >> stay;
