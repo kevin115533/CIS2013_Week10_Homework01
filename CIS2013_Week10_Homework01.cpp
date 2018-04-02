6fd3c3183108f3cf56e48484cfb5ded5cbc05085
kevin115533 <35547586+kevin115533@users.noreply.github.com>
2018-04-01 21:18:11 -0500

    week 10 homework
    trying to make a dynamic 2d array

--- CIS2013_Week10_Homework01.cpp
+++ CIS2013_Week10_Homework01.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
 int main() {
 	char stay;
-	int *a,*b;
-	int columns;
-	int rows;
+	int columns, rows;
+	int** field;
+	
+	//Trying a different approach to create a field with a dynamic 2d array
+	//its not working
 
 	cout << "Welcome to Minesweeper" << endl;
 	cout << "How many columns do you want the the field to be? ";
@@ -15,24 +16,55 @@ int main() {
 	cout << "How many rows do you want the field to be? ";
 	cin >> rows;
 
-	a = new int [columns];
-	b = new int[rows];
-	
-	cout << "  ";
-	for (int x = 0; x < columns; x++) {
-		cout << x+1 << " ";
+	field = new int*[columns];
+	for (int i = 0; i < columns; i++) {
+		field[i] = new int[rows];
 	}
-	cout << endl;
 
-	for (int x = 0; x < rows; x++) {
-		cout << x+1 << " ";
-		for (int y = 0; y < columns; y++) {
-			cout << ". ";
+	for (int i = 0; i < columns; i++) {
+		for (int j = 0; j < rows; i++) {
+			cin >> field[i][j]; // this line is producing some kind of error
 		}
-		cout << endl;
 	}
-	
 
+	for (int i = 0; i < columns; i++) {
+		for (int j = 0; j < rows; i++) {
+			cout << field[i][j];
+		}
+	}
+
+
+	/*cout << "How many mines do you want on the field? ";
+	cin >> mines;*/
+
+	/*a = new int [columns];
+	b = new int[rows];*/
+
+
+	
+	////prints field from the user inputs
+	//cout << "  ";
+	//for (int x = 0; x < columns; x++) {
+	//	cout << x+1 << " ";
+	//}
+	//cout << endl;
+
+	//for (int x = 0; x < rows; x++) {
+	//	cout << x+1 << " ";
+	//	for (int y = 0; y < columns; y++) {
+	//		cout << ". ";
+	//	}
+	//	cout << endl;
+	//}
+	//
+	////random placement of mines
+	//for (int x = 0; x < mines; x++) {
+
+	//}
+	//
+		
+		
+	delete[] field;
 	cin >> stay;
 	return 0;
 }
