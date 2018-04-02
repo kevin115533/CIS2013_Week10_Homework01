4ca1dbe932d86e66c3cd7a23c6978e8c8dbcd17e
kevin115533 <35547586+kevin115533@users.noreply.github.com>
2018-04-01 22:29:55 -0500

    week 10 homework
    worked a basic table layout using a dynamic array

--- CIS2013_Week10_Homework01.cpp
+++ CIS2013_Week10_Homework01.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 using namespace std;
 
+//Had a typo in a FOR loop that was causing issues but finally found after an hour
+
 int main() {
 	char stay;
 	int columns, rows;
-	int** field;
-	
-	//Trying a different approach to create a field with a dynamic 2d array
-	//its not working
+	int **field;
 
 	cout << "Welcome to Minesweeper" << endl;
 	cout << "How many columns do you want the the field to be? ";
@@ -21,49 +20,29 @@ int main() {
 		field[i] = new int[rows];
 	}
 
-	for (int i = 0; i < columns; i++) {
-		for (int j = 0; j < rows; i++) {
-			cin >> field[i][j]; // this line is producing some kind of error
-		}
-	}
 
 	for (int i = 0; i < columns; i++) {
-		for (int j = 0; j < rows; i++) {
-			cout << field[i][j];
+		for (int j = 0; j < rows; j++) {
+			field[i][j]=0; 
 		}
 	}
-
-
-	/*cout << "How many mines do you want on the field? ";
-	cin >> mines;*/
-
-	/*a = new int [columns];
-	b = new int[rows];*/
-
-
 	
-	////prints field from the user inputs
-	//cout << "  ";
-	//for (int x = 0; x < columns; x++) {
-	//	cout << x+1 << " ";
-	//}
-	//cout << endl;
+	cout << "  ";
+	for (int i = 0; i < columns; i++) {
+		cout << i << " ";
+	}
+	cout << endl;
 
-	//for (int x = 0; x < rows; x++) {
-	//	cout << x+1 << " ";
-	//	for (int y = 0; y < columns; y++) {
-	//		cout << ". ";
-	//	}
-	//	cout << endl;
-	//}
-	//
-	////random placement of mines
-	//for (int x = 0; x < mines; x++) {
+	for (int i = 0; i < columns; i++) {
+		cout << i << " ";
+		for (int j = 0; j < rows; j++) {
+			cout << field[i][j] << " ";
+		}
+		cout << endl;
+	}
 
-	//}
-	//
-		
-		
+
+
 	delete[] field;
 	cin >> stay;
 	return 0;
